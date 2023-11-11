a603f2a7b8ba1e8ca1a2ff4feabd7c6b02f80771
efg.river@gmail.com <efg.river@gmail.com>
2023-11-11 16:41:10 +0900

    update src/modules/config.ts

--- scripts/reviewer/src/modules/config.ts
+++ scripts/reviewer/src/modules/config.ts
@@ -3,6 +3,10 @@ export interface ReviewerAppConfig {
     GITHUB_TOKEN: string
 }
 export default function loadConfig() :ReviewerAppConfig {
+    if (process.env.OPENAI_API_KEY == undefined || process.env.OPENAI_API_KEY == "") {
+        console.log("env OPENAI_API_KEY should be set")
+        process.exit(2)
+    }
     return {
         OPENAI_API_KEY: `${process.env.OPENAI_API_KEY}`,
         GITHUB_TOKEN: `${process.env.GITHUB_TOKEN}`,
