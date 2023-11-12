82067ab07bf8f3c84cfe6d5a98ea62cb2eb06562
藤川英悟 <efg.river@gmail.com>
2023-11-12 09:38:00 +0900

    Update page.tsx

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-export default function PostDetailPage() {
-  let message = "post"
+import { useRouter } from 'next/router'
 
+export default function PostDetailPage() {
+  const router = useRouter()
+  let message = "post"
 
   /** Warning: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`. at input at form at main */
   return (
     <main>
-      <div>{message}</div>
+      <div>{message} {router.query.id}</div>
     </main>
   )
 }
