0e02f37eb6a53771bbebedc1330e074166db66e9
藤川英悟 <efg.river@gmail.com>
2023-11-12 09:24:43 +0900

    Update page.tsx

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,1 +1,13 @@
+'use client';
 
+export default function PostDetailPage() {
+  let message = "post"
+
+
+  /** Warning: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`. at input at form at main */
+  return (
+    <main>
+      <div>{message}</div>
+    </main>
+  )
+}
